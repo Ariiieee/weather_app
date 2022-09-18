6a718e91de6e2bb269a39897e0e2f81bb7040e3a
Oluwafunmilayo Dada <oluwafunmilayodada2018@gmail.com>
2022-09-19 00:15:35 +0100

    updated files
    added new features to the html, CSS and Js file

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,89 @@
 "use strict";
- 
+const temperature = document.querySelector(".temperature-degree");
+const celsiusUnit = document.querySelector(".celsius");
+const fahUnit = document.querySelector(".fah");
+const city = document.querySelector(".city");
+const hour = document.querySelector(".hr");
+const min = document.querySelector(".min");
+const day = document.querySelector(".day");
+const date = document.querySelector(".date");
+const month = document.querySelector(".month");
+const year = document.querySelector(".year");
+const weatherIcon = document.querySelector(".icon");
+const weatherIconDesc = document.querySelector(".icon-desc");
+const form = document.querySelector("form");
+const weatherInput = document.querySelector("input");
+const searchBtn = document.querySelector(".searchButton");
+
+const weatherStatCloudyData = document.querySelector(".cloudy-data");
+
+const humidityData = document.querySelector(".humidity-data");
+const windData = document.querySelector(".wind-data");
+const rainData = document.querySelector(".rain-data");
+
+let currentDate = new Date();
+const currentHr = currentDate.getHours();
+hour.innerHTML = currentHr;
+
+const currentMin = currentDate.getMinutes();
+min.innerHTML = currentMin;
+
+//days of the week
+let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+const dayResult = days.map((i, day) => (day, i));
+
+const currentDay = currentDate.getDay();
+day.innerHTML = dayResult[currentDay];
+
+//months
+let months = [
+  "Jan",
+  "Feb",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Oct",
+  "NOv",
+  "Dec",
+];
+
+const monthInWords = months.map((i, month) => (month, i));
+
+const currentMonth = currentDate.getMonth();
+month.innerHTML = monthInWords[currentMonth];
+
+const currentYear = currentDate.getFullYear();
+year.innerHTML = currentYear;
+
+const ShowWeatherData = (e) => {
+  e.preventDefault();
+  const weatherInputVal = weatherInput.value;
+  city.innerHTML = weatherInputVal;
+};
+form.addEventListener("submit", ShowWeatherData);
+
+const handleGetData = (res) => {
+  console.log(res.data);
+  temperature.innerHTML = Math.round(res.data.main.temp);
+  weatherIcon.innerHTML = res.data.weather[0].icon;
+  weatherIconDesc.innerHTML = res.data.weather[0].description;
+  humidityData.innerHTML = res.data.main.humidity + "%";
+ windData.innerHTML = Math.round(res.data.wind.speed) + "Km/hr";
+    city.innerHTML = res.data.name;
+    weatherIcon.setAttribute("src", ` http://openweathermap.org/img/wn/10d@2x.png`);
+    weatherIcon.setAttribute("alt", res.data.weather[0].description)
+};
+const apiKey = "203fa770242fcd2b9555d832a88ea567";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&units=metric&appid=${apiKey}`;
+
+axios
+  .get(apiUrl)
+  .then((res) => handleGetData(res))
+  .catch((err) => console.log(err));
+
+//   src="http://openweathermap.org/img/wn/${
+//     forecastDay.weather[0].icon
+// }@2x.png"
