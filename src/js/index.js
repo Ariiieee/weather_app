efabe37683c63b514983d0133944426b35014774
Oluwafunmilayo Dada <oluwafunmilayodada2018@gmail.com>
2022-09-19 01:46:45 +0100

    temperature conversion on celsius and Fahrenheit

--- src/js/index.js
+++ src/js/index.js
@@ -11,15 +11,13 @@ const month = document.querySelector(".month");
 const year = document.querySelector(".year");
 const weatherIcon = document.querySelector(".icon");
 const weatherIconDesc = document.querySelector(".icon-desc");
-const form = document.querySelector("form");
+const form = document.querySelector(".weather-form");
 const weatherInput = document.querySelector("input");
+const cityInput = document.querySelector("#search-city-input");
 const searchBtn = document.querySelector(".searchButton");
-
 const weatherStatCloudyData = document.querySelector(".cloudy-data");
-
 const humidityData = document.querySelector(".humidity-data");
 const windData = document.querySelector(".wind-data");
-const rainData = document.querySelector(".rain-data");
 
 let currentDate = new Date();
 const currentHr = currentDate.getHours();
@@ -58,32 +56,51 @@ month.innerHTML = monthInWords[currentMonth];
 const currentYear = currentDate.getFullYear();
 year.innerHTML = currentYear;
 
-const ShowWeatherData = (e) => {
-  e.preventDefault();
-  const weatherInputVal = weatherInput.value;
-  city.innerHTML = weatherInputVal;
-};
-form.addEventListener("submit", ShowWeatherData);
-
 const handleGetData = (res) => {
-  console.log(res.data);
-  temperature.innerHTML = Math.round(res.data.main.temp);
+  celsiusTemperature = res.data.main.temp;
+
+  temperature.innerHTML = Math.round(celsiusTemperature);
   weatherIcon.innerHTML = res.data.weather[0].icon;
   weatherIconDesc.innerHTML = res.data.weather[0].description;
   humidityData.innerHTML = res.data.main.humidity + "%";
- windData.innerHTML = Math.round(res.data.wind.speed) + "Km/hr";
-    city.innerHTML = res.data.name;
-    weatherIcon.setAttribute("src", ` http://openweathermap.org/img/wn/10d@2x.png`);
-    weatherIcon.setAttribute("alt", res.data.weather[0].description)
+  windData.innerHTML = Math.round(res.data.wind.speed) + "Km/hr";
+  city.innerHTML = res.data.name;
+  weatherIcon.setAttribute(
+    "src",
+    ` http://openweathermap.org/img/wn/${res.data.weather[0].icon}@2x.png`
+  );
+  weatherIcon.setAttribute("alt", res.data.weather[0].description);
 };
-const apiKey = "203fa770242fcd2b9555d832a88ea567";
-const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisbon&units=metric&appid=${apiKey}`;
 
-axios
-  .get(apiUrl)
-  .then((res) => handleGetData(res))
-  .catch((err) => console.log(err));
+function searchCity(city) {
+  const apiKey = "203fa770242fcd2b9555d832a88ea567";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-//   src="http://openweathermap.org/img/wn/${
-//     forecastDay.weather[0].icon
-// }@2x.png"
+  axios
+    .get(apiUrl)
+    .then((res) => handleGetData(res))
+    .catch((err) => console.log(err));
+}
+
+const handleSubmitSearchCity = (e) => {
+  e.preventDefault();
+  searchCity(cityInput.value);
+};
+form.addEventListener("submit", handleSubmitSearchCity);
+
+function displayCelsiusTemp(e) {
+  e.preventDefault();
+  temperature.innerHTML = Math.round(celsiusTemperature);
+}
+
+function displayFahTemp(e) {
+  e.preventDefault();
+  let fahTemp = (celsiusTemperature * 9) / 5 + 32;
+  temperature.innerHTML = Math.round(fahTemp);
+}
+
+let celsiusTemperature = null;
+
+celsiusUnit.addEventListener("click", displayCelsiusTemp);
+fahUnit.addEventListener("click", displayFahTemp);
+searchCity("Lagos")
